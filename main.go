af5bbbd3a1e0ebf588225605e80e53011fbffe45
Lochit Vinay <lochitvinay111@gmail.com>
2026-03-21 14:41:26 +0530

    Refactor routes to use handler functions

--- main.go
+++ main.go
@@ -1,21 +1,14 @@
-package main  
-import "github.com/gin-gonic/gin"  
-func main() { 
-	r := gin.Default()  
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "CivicTrack API running 🚀",
-		})
-	})
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"status": "running backend",
-		})
-	})
-	r.GET("/login", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"credentials": "username and password",
-		})
-	})
+package main
+
+import (
+	"civictrack/internal/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", handlers.HealthCheck)
+
 	r.Run(":8080")
 }
